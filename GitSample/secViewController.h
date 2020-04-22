54a0d9fdf7b066ca4bc7fcf0f3764f64a730e8ec
sudhanIOS <whari17@gmail.com>
2020-04-22 19:56:18 +0530

    new branch

---
+++ GitSample/secViewController.h
@@ -0,0 +1,17 @@
+//
+//  secViewController.h
+//  GitSample
+//
+//  Created by HariharasSudhan on 22/04/20.
+//  Copyright © 2020 Indium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface secViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
